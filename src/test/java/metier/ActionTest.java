66b6766cd4acf031f7211a99f82097d21f05f378
luqil <luqil@DESKTOP-3KV5Q3U>
2021-02-03 09:57:39 +0100

    Update of pom file

---
+++ src/test/java/metier/ActionTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metier;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author luqil
+ */
+public class ActionTest {
+    
+    public ActionTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
